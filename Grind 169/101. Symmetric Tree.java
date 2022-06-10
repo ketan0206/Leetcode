5943a78410119f7330daedf225556a17ff9bc9c0
ketan0206 <ketangoel2020@gmail.com>
2022-06-10 15:28:21 +0530

    week 2, day 3, question 9.

---
+++ Grind 169/101. Symmetric Tree.java
@@ -0,0 +1,22 @@
+/*
+Grind 169 
+https://www.techinterviewhandbook.org/grind75?difficulty=Easy&difficulty=Medium&difficulty=Hard&weeks=9&hours=17
+
+101. Symmetric Tree
+https://leetcode.com/problems/symmetric-tree/
+*/
+
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+    return root==null || isSymmetricHelp(root.left, root.right);
+}
+
+private boolean isSymmetricHelp(TreeNode left, TreeNode right){
+    if(left==null || right==null)
+        return left==right;
+    if(left.val!=right.val)
+        return false;
+    return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+}
+}
